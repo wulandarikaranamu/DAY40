3ef46cc019d561272f3ef868c1b1f9a6c14d3ea1
wulandarikaranamu <wulandarikaranamu02@gmail.com>
2022-01-20 21:01:16 +0700

    hayy

---
+++ day40.py
@@ -0,0 +1,17 @@
+#fungsi sum,lower,apper
+
+print("sum".center(60,"="))
+
+daftar_bil = [2,4,6,7,9]
+print(sum(daftar_bil))#tanpa parameter
+print(sum(daftar_bil,6))#dengan parameter
+
+print("lower".center(60,"="))
+
+huruf = "ABCDEFGH"
+print(huruf.lower())
+                    
+print("upper".center(60,"="))
+
+huruf = "abcdefg"
+print(huruf.upper())
